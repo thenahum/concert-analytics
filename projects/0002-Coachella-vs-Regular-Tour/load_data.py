470310fbc354371066edf7cb71e86949bc7dcdfb
Nahum Garcia <nahumgarcia@outlook.com>
2025-06-23 02:09:41 +0000

    creating load data for analysis

--- projects/0002-Coachella-vs-Regular-Tour/load_data.py
+++ projects/0002-Coachella-vs-Regular-Tour/load_data.py
@@ -5,8 +5,8 @@ import os
 PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../"))
 sys.path.insert(0, PROJECT_ROOT)
 
-from spotify import albums_dataframe,album_tracks_dataframe,albums_metadata_dataframe,tracks_metadata_dataframe
-from setlistfm import setlist_dataframe
+from spotify import search_artist_uri,albums_dataframe,album_tracks_dataframe,albums_metadata_dataframe,tracks_metadata_dataframe
+from setlistfm import setlist_dataframe,search_artist
 from postgres import load_dataframe,upsert_raw_table
 
 import logging
@@ -15,39 +15,58 @@ import logging
 logging.basicConfig(level=logging.INFO)  # DEBUG < INFO < WARNING < ERROR < CRITICAL
 
 
-mbid = "759b5ff1-91fe-4ec9-b9b7-75b7b2ceb283"
-spotifyuri="spotify:artist:3D4qYDvoPn5cQxtBm4oseo"
-name_hint="mewithoutyou"
-setlistfm_force_refresh = False
-spotify_force_refresh = False
-sample = False
+# print(search_artist("Turnstile"))
+# print(search_artist_uri("Turnstile")) 
 
-# print(setlist_dataframe(mbid,name_hint=name_hint,force_refresh=setlistfm_force_refresh,sample=False))
+fetch_config = {
+    'setlistfm_force_refresh': False
+    ,'spotify_force_refresh': False
+    ,'sample':True
+}
 
-# print(albums_dataframe(spotifyuri,name_hint=name_hint,force_refresh=spotify_force_refresh))
+tame_impala_fetch = {
+    'mbid': "63aa26c3-d59b-4da4-84ac-716b54f1ef4d"
+    ,'spotifyuri':"spotify:artist:5INjqkS1o8h1imAzPqGZBb"
+    ,'name_hint':"TameImpala"
+}
 
+billie_Eilish_fetch = {
+    'mbid': "f4abc0b5-3f7a-4eff-8f78-ac078dbce533"
+    ,'spotifyuri':"spotify:artist:6qqNVTkY8uBg9cP3Jd7DAH"
+    ,'name_hint':"BillieEilish"
+}
 
-#load track data
-# print(album_tracks_dataframe(spotifyuri,name_hint=name_hint,force_refresh=spotify_force_refresh))
+Japanese_Breakfast_fetch = {
+    'mbid': "8c529495-91f5-4e2f-b71b-adcb66878d04"
+    ,'spotifyuri':"spotify:artist:7MoIc5s9KXolCBH1fy9kkw"
+    ,'name_hint':"JapaneseBreakfast"
+}
 
-#load album metadata
-# print(albums_metadata_dataframe(spotifyuri,name_hint=name_hint,force_refresh=spotify_force_refresh))
+Turnstile_fetch = {
+    'mbid': "7b748dac-f5ce-45a7-9b95-c1d8b5b013ed"
+    ,'spotifyuri':"spotify:artist:2qnpHrOzdmOo1S4ox3j17x"
+    ,'name_hint':"Turnstile"
+}
 
-#load track metadata
-# print(tracks_metadata_dataframe(spotifyuri,name_hint=name_hint,force_refresh=spotify_force_refresh))
+project_artist_fetch = [
+    tame_impala_fetch
+    ,billie_Eilish_fetch
+    ,Japanese_Breakfast_fetch
+    ,Turnstile_fetch
+]
 
+for artist in project_artist_fetch:
+    #Setlist History
+    upsert_raw_table("setlist_history",setlist_dataframe(artist['mbid'],name_hint=artist['name_hint'],force_refresh=fetch_config['setlistfm_force_refresh'],sample=fetch_config['sample']))
 
+    #load album data
+    upsert_raw_table("artist_albums",albums_dataframe(artist['spotifyuri'],name_hint=artist['name_hint'],force_refresh=fetch_config['spotify_force_refresh']),)
 
-upsert_raw_table("setlist_history",setlist_dataframe(mbid,name_hint=name_hint,force_refresh=setlistfm_force_refresh,sample=sample))
+    #load track data
+    upsert_raw_table("artist_tracks",album_tracks_dataframe(artist['spotifyuri'],name_hint=artist['name_hint'],force_refresh=fetch_config['spotify_force_refresh']))
 
-# # #load album data
-# upsert_raw_table("artist_albums",albums_dataframe(spotifyuri,name_hint=name_hint,force_refresh=spotify_force_refresh),)
+    #load album metadata
+    upsert_raw_table("artist_albums_metadata",albums_metadata_dataframe(artist['spotifyuri'],name_hint=artist['name_hint'],force_refresh=fetch_config['spotify_force_refresh']))
 
-# # #load track data
-# upsert_raw_table("artist_tracks",album_tracks_dataframe(spotifyuri,name_hint=name_hint,force_refresh=spotify_force_refresh))
-
-# # #load album metadata
-# upsert_raw_table("artist_albums_metadata",albums_metadata_dataframe(spotifyuri,name_hint=name_hint,force_refresh=spotify_force_refresh))
-
-# # #load track metadata
-# upsert_raw_table("artist_tracks_metadata",tracks_metadata_dataframe(spotifyuri,name_hint=name_hint,force_refresh=spotify_force_refresh))
+    #load track metadata
+    upsert_raw_table("artist_tracks_metadata",tracks_metadata_dataframe(artist['spotifyuri'],name_hint=artist['name_hint'],force_refresh=fetch_config['spotify_force_refresh']))
