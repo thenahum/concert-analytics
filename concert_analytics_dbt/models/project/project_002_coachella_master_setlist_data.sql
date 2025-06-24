3ba55c5449aa2f1b5dcf8737c01bec3d67df705e
Nahum Garcia <nahumgarcia@outlook.com>
2025-06-23 22:46:32 -0700

    fixes

--- concert_analytics_dbt/models/project/project_002_coachella_master_setlist_data.sql
+++ concert_analytics_dbt/models/project/project_002_coachella_master_setlist_data.sql
@@ -3,21 +3,21 @@
 with coachella_dates_cte(artist_name_hint,coachella_weekend,coachella_start_date,coachella_end_date) as (
     values 
         ('Turnstile','Weekend 1','2022-04-16'::date,'2022-04-18'::date)
-        ,('BillieEilish','Weekend 1','2022-04-16'::date,'2022-04-18'::date)
-        ,('JapaneseBreakfast','Weekend 1','2022-04-16'::date,'2022-04-18'::date)
-        ,('TameImpala','Weekend 1','2019-04-12'::Date, '2019-04-14'::date)
-        ,('Turnstile','Weekend 2','2022-04-23'::Date, '2022-04-25'::date)
-        ,('BillieEilish','Weekend 2','2022-04-23'::Date, '2022-04-25'::date)
-        ,('JapaneseBreakfast','Weekend 2','2022-04-23'::Date, '2022-04-25'::date)
-        ,('TameImpala','Weekend 2','2019-04-19'::date, '2019-04-21'::Date)
+        , ('BillieEilish','Weekend 1','2022-04-16'::date,'2022-04-18'::date)
+        , ('JapaneseBreakfast','Weekend 1','2022-04-16'::date,'2022-04-18'::date)
+        , ('TameImpala','Weekend 1','2019-04-12'::Date, '2019-04-14'::date)
+        , ('Turnstile','Weekend 2','2022-04-23'::Date, '2022-04-25'::date)
+        , ('BillieEilish','Weekend 2','2022-04-23'::Date, '2022-04-25'::date)
+        , ('JapaneseBreakfast','Weekend 2','2022-04-23'::Date, '2022-04-25'::date)
+        , ('TameImpala','Weekend 2','2019-04-19'::date, '2019-04-21'::Date)
 )
 , analysis_dates_cte as (
     select 
         artist_name_hint
-        ,min(coachella_start_date) as first_coachella_date
-        ,max(coachella_end_date) as last_coachella_date
-        ,min(coachella_start_date) - 365 as reporting_start_date
-        ,max(coachella_end_date) + 365 as reporting_end_date
+        , min(coachella_start_date) as first_coachella_date
+        , max(coachella_end_date) as last_coachella_date
+        , min(coachella_start_date) - 365 as reporting_start_date
+        , max(coachella_end_date) + 365 as reporting_end_date
     from 
         coachella_dates_cte
     group by 
@@ -25,20 +25,51 @@ with coachella_dates_cte(artist_name_hint,coachella_weekend,coachella_start_date
 )
 , setlisth_history_coachella_flags_cte as (
     select 
-        msh.artist_name_hint	
-        ,msh.event_set_song_id	
-        ,msh.event_id	
-        ,msh.event_date	
-        ,msh.event_info	
-        ,msh.event_url	
-        ,msh.event_tour_id	
-        ,msh.event_tour	
-        ,msh.venue_id	
-        ,msh.venue_name	
-        ,case when cd_cte.artist_name_hint is not null then TRUE else FALSE end as is_coachella
-        ,cd_cte.coachella_weekend as coachella_weekend
-        ,case when ad_cte.first_coachella_date > msh.event_date then  ad_cte.first_coachella_date - msh.event_date end as days_before_first_coachella_date
-        ,case when ad_cte.last_coachella_date < msh.event_date then msh.event_date - ad_cte.last_coachella_date end as days_after_last_coachella_date
+        msh.artist_name_hint
+        , msh.event_set_song_id
+        , msh.event_id
+        , msh.event_date
+        , msh.event_info
+        , msh.event_url
+        , msh.event_tour_id
+        , msh.event_tour
+        , msh.venue_id
+        , msh.venue_name
+        , msh.venue_city
+        , msh.venue_state_code
+        , msh.venue_country_code
+        , msh.venue_latitude
+        , msh.venue_longitude
+        , msh.set_index
+        , msh.set_index_reversed
+        , msh.encore_index
+        , msh.encore_flag
+        , msh.song_index
+        , msh.song_index_reversed
+        , msh.song_position_in_set_index
+        , msh.song_position_in_set_index_reversed
+        , msh.song_name
+        , msh.song_info
+        , msh.song_cover_flag
+        , msh.song_cover_artist_mbid
+        , msh.song_cover_artist_name
+        , msh.song_with_flag
+        , msh.song_with_artist_mbid
+        , msh.song_with_artist_name
+        , msh.song_last_event_set_song_id
+        , msh.song_last_event_id
+        , msh.song_last_event_date
+        , case 
+            when cd_cte.artist_name_hint is not null then TRUE 
+            else FALSE 
+        end as is_coachella
+        , cd_cte.coachella_weekend as coachella_weekend
+        , case 
+            when ad_cte.first_coachella_date > msh.event_date then  ad_cte.first_coachella_date - msh.event_date 
+        end as days_before_first_coachella_date
+        , case 
+            when ad_cte.last_coachella_date < msh.event_date then msh.event_date - ad_cte.last_coachella_date 
+        end as days_after_last_coachella_date
     from 
         {{ ref('mart_setlist_history') }} as msh
         join analysis_dates_cte as ad_cte
@@ -54,26 +85,33 @@ with coachella_dates_cte(artist_name_hint,coachella_weekend,coachella_start_date
         *
     from 
         {{ ref('mart_track_setlist_similarity_scores') }} as mtsss
-    where 
-        mtsss.similarity_rank = 1
+    where TRUE
+        and mtsss.similarity_rank = 1
 )
 select
     cs_cte.*
-    ,tr.album_id	
-    ,tr.album_url	
-    ,tr.album_uri	
-    ,tr.album_type	
-    ,tr.album_name	
-    ,tr.album_total_tracks	
-    ,tr.album_release_date	
-    ,tr.album_image_url	
-    ,tr.album_popularity	
-    ,tr.track_id	
-    ,tr.track_url	
-	,es.event_total_songs
-	,es.event_total_sets
-	,es.event_total_encore_songs
-	,es.event_total_non_encore_songs
+    , tr.album_id
+    , tr.album_url
+    , tr.album_uri
+    , tr.album_type
+    , tr.album_name
+    , tr.album_total_tracks
+    , tr.album_release_date
+    , tr.album_image_url
+    , tr.album_popularity
+    , tr.track_id
+    , tr.track_url
+    , tr.track_uri
+    , tr.track_disk_number
+    , tr.track_duration_seconds
+    , tr.track_name
+    , tr.track_number
+    , tr.track_popularity
+    , tr.track_irsc
+	, es.event_total_songs
+	, es.event_total_sets
+	, es.event_total_encore_songs
+	, es.event_total_non_encore_songs
 from 
     setlisth_history_coachella_flags_cte as cs_cte
     left join track_link_filtered_cte as tl_cte
